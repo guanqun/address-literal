d3d6ff5d07f263580b7007e0489cfe712ed0762a
Guanqun Lu <guanqunlu@outlook.com>
2021-07-04 11:02:37 +0800

    run cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
-use proc_macro::TokenStream;
-use syn::{parse_macro_input, LitStr};
 use primitive_types::H160;
+use proc_macro::TokenStream;
 use std::str::FromStr;
+use syn::{parse_macro_input, LitStr};
 
 #[proc_macro]
 pub fn addr(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as LitStr);
-    let address = H160::from_str(input.value().as_str()).expect("the string can't be parsed as Address");
-    let bytes: Vec<String> = address.as_fixed_bytes().into_iter().map(|b| format!("{}u8", b)).collect();
-    format!("primitive_types::H160::from([{}])", bytes.join(",")).parse().unwrap()
+    let address =
+        H160::from_str(input.value().as_str()).expect("the string can't be parsed as Address");
+    let bytes: Vec<String> = address
+        .as_fixed_bytes()
+        .into_iter()
+        .map(|b| format!("{}u8", b))
+        .collect();
+    format!("primitive_types::H160::from([{}])", bytes.join(","))
+        .parse()
+        .unwrap()
 }
